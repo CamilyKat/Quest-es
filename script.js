806e78a79c4fcf36ea1795b8cdff2368eda2fa5c
CamilyKat <138224221+CamilyKat@users.noreply.github.com>
2025-07-02 10:43:01 -0300

    .

---
+++ script.js
@@ -0,0 +1,83 @@
+{
+Enunciado: "Você recebeu seu salário, qual a primeira coisa que você faz?",
+alternativas: [
+{
+texto:"Gasto em despesas necessárias e guardo para o futuro",
+afirmação: ""
+},
+{
+texto:"Gasto em coisas que eu queria comprar, mas não são necessárias",
+afirmação: " "
+}
+]
+},
+
+{
+Enunciado: "",
+alternativas: [
+{
+texto:"",
+afirmação: ""
+},
+{
+texto:"",
+afirmação: ""
+}
+]
+},
+
+{
+Enunciado: "",
+alternativas: [
+{
+texto:"",
+afirmação: ""
+},
+{
+texto:"",
+afirmação: ""
+}
+]
+},
+
+{
+Enunciado: "",
+alternativas: [
+{
+texto:"",
+afirmação: ""
+},
+{
+texto:"",
+afirmação: ""
+}
+]
+},
+
+{
+Enunciado: "",
+alternativas: [
+{
+texto:"",
+afirmação: ""
+},
+{
+texto:"",
+afirmação: ""
+}
+]
+},
+
+{
+Enunciado: "",
+alternativas: [
+{
+texto:"",
+afirmação: ""
+},
+{
+texto:"",
+afirmação: ""
+}
+]
+},
