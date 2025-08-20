12e3d8b92e7a655af3371b78eed5ec7a434d05a1
CamilyKat <138224221+CamilyKat@users.noreply.github.com>
2025-08-20 13:43:43 +0000

    .

--- script.js
+++ script.js
@@ -1,83 +1,84 @@
 {
-Enunciado: "Você recebeu seu salário, qual a primeira coisa que você faz?",
+Enunciado: "Você recebeu seu salário. O que você faz primeiro?",
 alternativas: [
 {
-texto:"Gasto em despesas necessárias e guardo para o futuro",
-afirmação: ""
+texto:"Compro algo que queria há muito tempo, mesmo que fique apertado depois",
+afirmação: "Nos primeiros dias, você compra roupas novas, sai para comer fora e aproveita as promoções, porém no final do mês as coisas ficam mais apertadas"
 },
 {
-texto:"Gasto em coisas que eu queria comprar, mas não são necessárias",
-afirmação: " "
+texto:"Organizo minhas despezas fixas, guardo uma parte e só depois vejo o que posso gastar",
+afirmação: "Você mantêm controle de suas contas e despesas"
 }
 ]
 },
 
 {
-Enunciado: "",
+Enunciado: "Você costuma anotar ou controlar seus gastos?",
 alternativas: [
 {
-texto:"",
+texto:"Não, só olho quando percebo que o dinheiro está acabando",
 afirmação: ""
 },
 {
-texto:"",
-afirmação: ""
-}
-]
-},
-
-{
-Enunciado: "",
-alternativas: [
-{
-texto:"",
-afirmação: ""
-},
-{
-texto:"",
+texto:"Sim, tenho uma planilha ou aplicativo para acompanhar tudo",
 afirmação: ""
 }
 ]
 },
 
 {
-Enunciado: "",
+Enunciado: "Aparece uma promoção imperdível de celular. O que você faz?",
 alternativas: [
 {
-texto:"",
+texto:"Compro na hora, mesmo que precise parcelar ou pegar dinheiro emprestado",
 afirmação: ""
 },
 {
-texto:"",
+texto:"Avalio se realmente preciso, se eu possso pagar e se não vai atrapalhar o orçamento",
 afirmação: ""
 }
 ]
 },
 
 {
-Enunciado: "",
+Enunciado: "Você costuma guardar dinheiro todo mês?",
 alternativas: [
 {
-texto:"",
+texto:"Não sobra nada para guarda, acabo gastando tudo",
 afirmação: ""
 },
 {
-texto:"",
+texto:"Sim, guardar é prioridade, nem que seja um valor pequeno",
 afirmação: ""
 }
 ]
 },
 
 {
-Enunciado: "",
+Enunciado: "Como voê lida com emergências financeiras?",
 alternativas: [
 {
-texto:"",
+texto:"Fico desesperado e uso o cartão ou pego dinheiro emprestado",
 afirmação: ""
 },
 {
-texto:"",
+texto:"Uso a minha reserva de emerência ou reorganizo o orçamento para resolver",
 afirmação: ""
 }
 ]
 },
+
+{
+Enunciado: "Você tem algum plano financeiro para o futuro",
+alternativas: [
+{
+texto:"Não penso muito no futuro, prefiro aproveitar o agora",
+afirmação: ""
+},
+{
+texto:"Tenho objetivos claros e estou me preparado aos poucos para alcança-los",
+afirmação: ""
+}
+]
+},
+
